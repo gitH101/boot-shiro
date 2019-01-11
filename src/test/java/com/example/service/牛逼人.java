aa64a2ed95413468d483459f7f5951a417564201
zhaowenwen <1773318302@qq.com>
2019-01-11 18:49:35 +0800

    shading sphere

---
+++ src/test/java/com/example/service/牛逼人.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+public class 牛逼人 {
+
+    public void 我真牛逼啊() {
+        System.out.println("不牛逼不行啊");
+    }
+
+    public static void main(String[] args) {
+        牛逼人 牛逼人 = new 牛逼人();
+        牛逼人.我真牛逼啊();
+    }
+
+}
